6907fb84c6890145774965a2158c665510126d29
mariajsan <60493676+mariajsan@users.noreply.github.com>
2020-06-05 16:47:13 -0700

    proyecto

---
+++ Proyecto_Final/Proyecto_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Web.Startup))]
+namespace Proyecto_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
